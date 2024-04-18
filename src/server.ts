e5b7e9fbaaf2913ef9d68c49cb8ed9efd9504ea0
Boris Dimitrov <boris1234dimitrov@gmail.com>
2024-04-18 23:20:33 +0300

    Added uploading and downloading

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,71 @@
 import * as fs from 'fs';
 import { createServer } from 'node:http';
 import formidable from 'formidable';
-import { fileURLToPath } from 'url';
 
-const dir_path: string = import.meta.url;
+const dirPath: string = process.cwd();
 const hostname : string = '127.0.0.1';
 const port : number = 3000;
 
-const server = createServer((req, res) => {
-  if (req.url == '/fileupload') {
+var storedFileData : Buffer[] = [];
+var storedFileNames : string[] = [];
+
+function getPageHtml() : string {
+  return `
+  <h2>Personal Cloud</h2>
+  <div>Stored files: ${storedFileNames.toString()}</div>
+  <form action="/upload" enctype="multipart/form-data" method="post">
+    <div>File: <input type="file" name="multipleFiles" multiple="multiple" /></div>
+    <input type="submit" value="Upload" />
+  </form>
+  <form action="/download" method="post">
+    <input type="submit" value="Download" />
+  </form>
+  `;
+}
+
+const server = createServer(async (req, res) => {  
+  if (req.url === '/upload' && req.method.toLowerCase() === 'post') {
+    // parse a file upload
     const form = formidable({});
-    form.parse(req, function (err, fields, files) {
-      res.write("File uploaded!");
-      res.end();
-      // Save the file to the root directory (files.filetoupload.originalFilename is undefined for some reason)
-      /*var oldpath : string = files.filetoupload.filepath;
-      var newpath : string = dir_path + files.filetoupload.originalFilename;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });*/
-    });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
+    let fields;
+    let files;
+    try {
+        [fields, files] = await form.parse(req);
+    } catch (err) {
+        console.error(err);
+        res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
+        res.end(String(err));
+        return;
+    }
+
+    for(let i = 0; i < files.multipleFiles.length; ++i)
+    {
+      let file = files.multipleFiles[i];
+      var oldpath = file.filepath;
+      storedFileNames.push(file.originalFilename);
+      fs.readFile(file.filepath, function (err, data){
+        storedFileData.push(data);
+      });
+      fs.unlink(oldpath, (err) => {
+        if(err) throw err;
+      });
+    }
+    res.end(getPageHtml());
+  }
+  else if(req.url === "/download")
+  {
+    for(let i = 0; i < storedFileNames.length; ++i) {
+      let fileName = storedFileNames[i];
+      console.log(fileName);
+      fs.writeFile(dirPath + `\\${fileName}`, storedFileData[i], function (err){
+        if(err) throw err;
+      });
+    }
+    res.end(getPageHtml());
+  }
+  else {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(getPageHtml());
   }
 });
 
