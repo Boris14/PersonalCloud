1e45fe1dde878cdef255b9484effc5f57506b06e
mfrosina <frosinamulachkaa@gmail.com>
2024-05-12 16:45:24 +0200

    Added the login page for the personal cloud project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+import Navbar from './client/components/Navbar';
+import LoginForm, { LoginData } from './client/components/LoginForm';
+import Home from './client/components/Home';
+import Dashboard from './client/components/Dashboard';
+import YourFiles from './client/components/YourFiles';
+import SharedWithMe from './client/components/SharedWithMe';
+import RegistrationForm, { RegistrationData } from './client/components/RegistrationForm';
+
+import { handleLogin } from './services/loginService';
+import { handleRegister } from './services/registerService';
+import { UserData, users } from './server/server';
+
+
+
+const App: React.FC = () => {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userInfo, setUserInfo] = useState<UserData | null>(null);
+
+
+  const login = async (loginData: LoginData) => {
+    const userAuthData = await handleLogin(loginData);
+    //const user = users.find((u) => u.email === loginData.email);
+    //const userData = getUserData(loginData.email);
+    if (userAuthData) {
+      setLoggedIn(true);
+      //setUserInfo(user);
+      return true;
+    }
+    return false;
+  };
+
+
+  const logout = () => {
+    window.location.href = '/';
+    setLoggedIn(false);
+    setUserInfo(null);
+  };
+
+  const register = async (registrationData: RegistrationData) => {
+    const userData = await handleRegister(registrationData);
+    if (userData) {
+      const newUser: UserData = {
+        firstName: registrationData.firstName,
+        lastName: registrationData.lastName,
+        email: registrationData.email,
+        password: registrationData.password,
+      };
+
+      setLoggedIn(true);
+      setUserInfo(newUser);
+      return true;
+    }
+    return false;
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar loggedIn={loggedIn} handleLogout={logout} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={loggedIn ? <Navigate to="/dashboard" /> : <LoginForm onLogin={login} />} />
+          {userInfo && userInfo.firstName && userInfo.lastName ? (
+            <Route
+              path="/dashboard"
+              element={<Dashboard firstName={userInfo.firstName} lastName={userInfo.lastName} />}
+            />
+          ) : (
+            <Route path="/dashboard" element={<Dashboard />} />)
+          }
+          {loggedIn && (<Route path="/your-files" element={<YourFiles />} />)}
+          {loggedIn && (<Route path="/shared-with-me" element={<SharedWithMe />} />)}
+          <Route path="/register" element={loggedIn ? <Navigate to="/dashboard" /> : <RegistrationForm onRegister={register} />} />
+        </Routes>
+      </div>
+    </Router>
+
   );
 }
 
---
+++ src/client/components/SharedWithMe.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/yourfiles.css'
+
+const SharedWithMe: React.FC = () => {
+  return (
+    <div className="your-files-container">
+      <h2>Shared with Me</h2>
+    </div>
+  );
+};
+
+export default SharedWithMe;
